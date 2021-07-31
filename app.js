d215cec257dd591928a93649d008b28c954b90b4
CrystalMcCorkle <crystallmccorkle@gmail.com>
2021-07-30 21:59:49 -0500

    cleaning up some testing code

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const axios = require('axios');
 const key = process.env.PETFINDER_KEY;
 const secret = process.env.PETFINDER_SECRET;
 const accessToken = process.env.PETFINDER_ACCESS_TOKEN;
+const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
 
 
 mongoose.connect('mongodb://localhost:27017/myFurEverPal', {
@@ -42,87 +44,7 @@ app.get('/', (req, res) => {
 //     res.send(survey);
 // });
 
-app.get('/pets', async (req, res) => {
-    const pets = await Findpet.find({});
-    res.render('pets/index', { pets }); 
-})
 
-
-//setting the headers to use the access token 
-// const config = { 
-//     headers: { 
-//         Accept: 'application/json',
-//         'Content-Type': 'application/json',
-//         'Authorization': `Bearer ${accessToken}` 
-//     }
-// };
-
-
-//function to get all pets 
-// const getPets = async () => {
-//     const res = await axios.get('https://api.petfinder.com/v2/animals', config)
-//     console.log(res.data);
-//     //const { animals } = await res.json();
-   
-// }
-
-//calling the getPets function 
-// getPets();
-
-
-//testing to see if can render the data for a specific id 
-
-
-
-
-
-
-//app.get('/pets/:id', async (req, res) => {
-//     //res.render('/');
-// });
-
-
-
-
-
-
-
-//app.get('/index', async(req, res) => {
-// async function getPets() {
-//     try {
-//         const response = await axios.get(instance);
-//         //return await axios.get(`curl -H Authorization: Bearer ${accessToken} 'https://api.petfinder.com/v2/animals'`)
-//         console.log(response);
-//         const data = await response.json();
-//         console.log(data);
-//         res.render(data);
-//         //showPets(data.message);
-//     } catch(error) {
-//         console.error("Oh no! There was an error!", error);
-//     }
-// //});
-// };
-
-//getPets();
-
-
-//calling function to initiate
-//getPets();
-
-
-
-
-// const showPets = async() => {
-//     const pets = await getPets();
-//     if (pets){
-//         console.log(pets);
-//         //(data);
-//         console.log(message);
-//         console.log(instance);
-//     }
-// }
-
-// showPets();
 
 app.get('/index', async (req, res) => {
     try {
@@ -149,7 +71,8 @@ app.get('/index', async (req, res) => {
         )
         const { animals } = await petType.json(); 
         const [ { id, url, type, age, gender, description } ] = animals;
-        console.log({ id, url, type, age, gender, description }); //prints ONE object w/ one's pet's info 
+        console.log({ id, url, type, age, gender, description }); //prints ONE object w/ one's pet's info --just prints these items though (no photo) 
+        //console.log(animals[0]);
         //res.send({ animals }); //brings up the WHOLE entire OBJECT OF animals {animals:[{id: 123, url: etc}]}
         //res.send(animals);//this starts w/ the array and then has objects [{id: 123, url: etc.}]
         //res.send({ id, url, type, age, gender, description }); //brings up one pet's info only 
